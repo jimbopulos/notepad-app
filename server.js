cf5b01ceb12775c032e68fc89a4fd71cade3eb9b
James Galantino <james_galantino@aol.com>
2021-02-10 15:38:23 -0500

    added code to server.js

--- server.js
+++ server.js
@@ -7,3 +7,14 @@ const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('*', (req, res) => {
+    res.send('I am the greatest.')
+})
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
+
