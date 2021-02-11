8c9c3e308063a6332989f4b009a95294e53988cc
James Galantino <james_galantino@aol.com>
2021-02-10 20:59:58 -0500

    Added file return route for index.html

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'))
-});
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
