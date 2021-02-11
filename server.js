f7499161100ebe8e52297627dd0ce9c782a089bd
James Galantino <james_galantino@aol.com>
2021-02-10 22:20:04 -0500

    Routed index.html and notes.html

--- server.js
+++ server.js
@@ -17,10 +17,8 @@ app.use(express.json());
 //     res.send('Bitchass bitch');
 // })
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
