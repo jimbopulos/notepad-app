885ce26e5e15a4ab810a73ada1fd2032aabb1c08
James Galantino <james_galantino@aol.com>
2021-02-11 19:27:11 -0500

    Edited get route for index.html and notes.html

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 // Dependencies
-
 const express = require('express');
 const path = require('path');
 
@@ -10,16 +9,13 @@ const PORT = process.env.PORT || 3000;
 // Retrieve any static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public', 'notes.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
-
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
