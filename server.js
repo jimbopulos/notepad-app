c3bf3eef9b54f33cabcfbf80b459b9019028e094
James Galantino <james_galantino@aol.com>
2021-02-11 19:51:38 -0500

    Added readFile function and parse for db.json

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 // Dependencies
 const express = require('express');
 const path = require('path');
+const fs = require("fs");
+const db = fs.readFileSync("./db/db.json");
+
+// read and parse db.json content
+const rawData = JSON.parse(db);
+console.log(rawData);
 
 // Sets up the Express App
 const app = express();
@@ -13,9 +19,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// API Routes
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+// app.get('/notes', (req, res) => {
+//     res.json()
+// });
+
 // Routes
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, '/public/notes.html')));
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
+// PORT listener to serve the app
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
